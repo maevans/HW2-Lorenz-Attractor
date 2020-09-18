72425907f04f869ba0c090178f8c29a7da5ec609
Mahalia E <maev8907@colorado.edu>
2020-09-17 18:42:31 -0600

    Lorenz File

---
+++ HW2-Lorenz Attractor/Lorenz.c
@@ -0,0 +1,55 @@
+////
+////  main.c
+////  HW2-Lorenz Attractor
+////
+////  Created by Mahalia Evans on 9/17/20.
+////  Copyright © 2020 Mahalia Evans. All rights reserved.
+////
+
+/*
+ * Simple program to demonstrate generating coordinates
+ * using the Lorenz Attractor
+ */
+
+
+// Lorenz program provides an integrator that may be used to generate a sequence of points to use in the visualization.
+// At a minimum, your program should provide a line trace of the sequence of attractor and allow the trace to be viewed from different positions and allow one or more of the parameters in the Lorenz equations to be modified interactively.
+// The academic lesson here is what it takes to get a simple scene rendered and basic user interaction.
+
+#include <stdio.h>
+
+/*  Lorenz Parameters  */
+double s  = 10;
+double b  = 2.6666;
+double r  = 28;
+
+/*
+ *  Main
+ */
+int main(int argc, char *argv[])
+{
+   int i;
+   /*  Coordinates  */
+   double x = 1;
+   double y = 1;
+   double z = 1;
+   /*  Time step  */
+   double dt = 0.001;
+
+   printf("%5d %8.3f %8.3f %8.3f\n",0,x,y,z);
+   /*
+    *  Integrate 50,000 steps (50 time units with dt = 0.001)
+    *  Explicit Euler integration
+    */
+   for (i=0;i<50000;i++)
+   {
+      double dx = s*(y-x);
+      double dy = x*(r-z)-y;
+      double dz = x*y - b*z;
+      x += dt*dx;
+      y += dt*dy;
+      z += dt*dz;
+      printf("%5d %8.3f %8.3f %8.3f\n",i+1,x,y,z);
+   }
+   return 0;
+}
